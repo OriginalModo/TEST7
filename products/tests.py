a8c01754562f3e62593a77a92339de56158d5d4d
PK111 <1@>
2024-09-18 16:36:12 +0300

    add tests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Product
 
-# Create your tests here.
+
+class ProductTests(APITestCase):
+    def test_create_product(self):
+        url = reverse('product-list')
+        data = {'name': 'Test Product', 'description': 'This is a test product', 'price': 10.00}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertEqual(Product.objects.get().name, 'Test Product')
+
+    def test_get_products(self):
+        url = reverse('product-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
